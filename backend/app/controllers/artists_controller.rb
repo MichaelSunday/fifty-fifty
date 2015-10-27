5f6555f7400189d57482cadbc914da723ce41938
Michael Domingo <MichaelDomingo@Michaels-MacBook-Air.local>
2015-10-27 14:28:54 -0700

    created css file

--- backend/app/controllers/artists_controller.rb
+++ backend/app/controllers/artists_controller.rb
@@ -1,11 +1,7 @@
 class ArtistsController < ApplicationController
-	acts_as_votable
-	def index
-		@artist = Artist.new(:name => 'my artist!')
-		@artist.save
 
-		@artist.liked_by @user
-		@artist.votes_for.size # => 1
+	def index
+
 	end
 end
 
