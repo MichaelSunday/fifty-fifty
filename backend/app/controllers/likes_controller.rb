735fb1d8d65c866a6c34af37f9f448ff8e6463ea
Michael Domingo <MichaelDomingo@Michaels-MacBook-Air.local>
2015-10-27 09:59:26 -0700

    find related artist

--- backend/app/controllers/likes_controller.rb
+++ backend/app/controllers/likes_controller.rb
@@ -8,7 +8,16 @@ class LikesController < ApplicationController
 			end
 		artist_id = artist.id
 				like = Like.create(artist_id: artist_id, user_id: user_id)
-			render json:{:user_id => user_id, :artist_id => artist_id}
+			# render json:{:user_id => user_id, :artist_id => artist_id}
+				original_artist = RSpotify::Artist.find(spotify_id)	
+				related_artists = original_artist.related_artists
+				random_artist = related_artists.sample
+				albums = random_artist.albums
+				album = albums.sample
+				tracks = album.tracks
+				track = tracks.sample
+				render json: track
+
 	
 	end
 end
