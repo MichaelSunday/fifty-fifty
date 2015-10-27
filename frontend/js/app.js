77c0faf9183332d19837c020dc12a9be349e2ef6
Michael Domingo <MichaelDomingo@Michaels-MacBook-Air.local>
2015-10-27 15:18:29 -0700

    created genre controller

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,3 +1,28 @@
 angular
   .module('FiftyFifty', ['ui.router'])
   .config(FiftyRouter);
+
+  function CriminalRouter($stateProvider, $urlRouterProvider){
+
+  $urlRouterProvider.otherwise("/index");
+
+  $stateProvider
+  .state('index', {
+    url: '/genres',
+    templateUrl: 'genre.html'
+  })
+  .state('new', {
+    url: '/new',
+    templateUrl: 'new.html'
+  })
+  .state('about', {
+    url: '/about',
+    templateUrl: 'about.html'
+  })
+  .state('show', {
+    url: '/criminals/:id',
+    templateUrl: 'show.html'
+  });
+
+
+}
