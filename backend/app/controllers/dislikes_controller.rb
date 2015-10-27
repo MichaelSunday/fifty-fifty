1973b385e451aceae4d2b5c7a982bff79efa3e84
Michael Domingo <MichaelDomingo@Michaels-MacBook-Air.local>
2015-10-27 12:18:36 -0700

    dislike table

--- backend/app/controllers/dislikes_controller.rb
+++ backend/app/controllers/dislikes_controller.rb
@@ -1,5 +1,21 @@
 class DislikesController < ApplicationController
 	def index
-		
+		user_id = params[:user_id]
+		spotify_id = params[:spotify_id]
+		artist = Artist.find_by spotify_id: spotify_id
+			if !artist
+				artist = Artist.create(spotify_id: spotify_id)
+			end
+		artist_id = artist.id
+			dislike = Dislike.create(artist_id: artist_id, user_id: user_id)
+		original_artist = RSpotify::Artist.find(spotify_id)	
+		related_artists = original_artist.related_artists
+		random_artist = related_artists.sample
+		albums = random_artist.albums
+		album = albums.sample
+		tracks = album.tracks
+		track = tracks.sample
+		render json: track
 	end
 end
+
--- backend/app/controllers/likes_controller.rb
+++ backend/app/controllers/likes_controller.rb
@@ -9,14 +9,14 @@ class LikesController < ApplicationController
 		artist_id = artist.id
 				like = Like.create(artist_id: artist_id, user_id: user_id)
 			# render json:{:user_id => user_id, :artist_id => artist_id}
-				original_artist = RSpotify::Artist.find(spotify_id)	
-				related_artists = original_artist.related_artists
-				random_artist = related_artists.sample
-				albums = random_artist.albums
-				album = albums.sample
-				tracks = album.tracks
-				track = tracks.sample
-				render json: track
+		original_artist = RSpotify::Artist.find(spotify_id)	
+		related_artists = original_artist.related_artists
+		random_artist = related_artists.sample
+		albums = random_artist.albums
+		album = albums.sample
+		tracks = album.tracks
+		track = tracks.sample
+		render json: track
 
 	
 	end
